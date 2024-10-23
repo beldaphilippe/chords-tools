9389f9d78d10f5724debbf16778391ab896e2681
Philippe Belda <beldap@ensipc182.ensimag.fr>
2024-10-23 15:26:36 +0200

    a

--- README.md
+++ README.md
@@ -4,20 +4,31 @@
 
 ### Intervals
 
-...
+Major second = 2 semitones
+Minor third = 3 semitones
+Major third = 4 semitones
+Perfect fourth = 5 semitones
 
 ### 3 notes chords
 
-|| 3 notes chords         ||
 | Chord | Notes | Notation |
 |-------|-------|----------|
 | Major | 1, 3, 5 | C |
 | minor | 1, 3m, 5 | Cm |
 | disminished | 1, 3m, 5- | C- |
 | augmented | 1, 3, 5+ | C+ |
+| suspended | 1, 2, 5 | Csus2 |
 | suspended | 1, 4, 5 | Csus4 |
-|           | 1, 2, 5 | Csus2 |
+
+### 4 notes chords (seventh)
+
 | Chord | Notes | Notation |
 |-------|-------|----------|
+| dominant seventh | major + 7m | C7 |
+| major seventh | major + 7 | Cmaj7 |
+| minor seventh | minor + 7m | Cm7 |
+| minor seventh flat fifth | disminished + 7m | Cm7b5 |
 
+### 5 notes chords (ninth)
 
+...
--- get_chord.c
+++ get_chord.c
@@ -1,1 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void paste(char *src, char *dst) {
+    while (*src != '\0') {
+        *dst = *src;
+        dst++;
+        src++;
+    }
+}
+
+char **split(char *line, char delimiter) {
+    char **split_line = malloc(sizeof(char*)*5);
+    int i=0;
+    int w_n = 0;
+    while (line[i] != '\0') {
+        int w_start = i;
+        while (line[i] != delimiter) {
+            i++;
+        }
+        if (w_start-i>0) {
+            line[i] = '\0';
+            split_line[w_n] = malloc(sizeof(char)*(w_start-i+1));
+            paste(&line[w_start], split_line[w_n]);
+            line[i] = delimiter;
+            w_n ++;
+        }
+        else{
+            i++;
+        }
+    }
+    return split_line;
+}
+
+int main(){
+    printf("==== Chord detector ====\n");
+    printf("Give the notes of the chord, on a single line, separated by spaces :\n");
+    char line[71];
+    // char *line = malloc(sizeof(char)*10);
+    scanf(" %[^\n]", line);
+    char **notes = split(line, ' ');
+    printf("%s\n", line);
+    printf("%s , %s\n", notes[0], notes[1]);
+    return 0;
+}
