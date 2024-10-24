21583a706b9a58cea5410529f68a196a5aff8cb8
Philippe Belda <beldap@ensipc170.ensimag.fr>
2024-10-24 14:28:32 +0200

    ahah

--- get_chord.c
+++ get_chord.c
@@ -20,7 +20,7 @@ void tolowercase(char *word) {
   }
 }
 
-char **splitnlower(char *line, char delimiter) {
+char **split(char *line, char delimiter) {
   char **split_line = malloc(sizeof(char *) * 5);
   for (int i = 0; i < 5; i++)
     split_line[i] = NULL;
@@ -32,49 +32,55 @@ char **splitnlower(char *line, char delimiter) {
       i++;
     }
     if (w_start != i) {
+      int tmp = line[i];
       line[i] = '\0';
       split_line[w_n] = malloc(sizeof(char) * (i - w_start + 1));
       paste(&line[w_start], split_line[w_n]);
-      tolowercase(split_line[w_n]);
-      line[i] = delimiter;
+      line[i] = tmp;
       w_n++;
-    }
-    i++;
+    } else
+      i++;
   }
   return split_line;
 }
 
-char ***NOTES = {{"a"},        {"a#", "bb"}, {"b"},        {"c"},
-                 {"c#", "db"}, {"d"},        {"d#", "eb"}, {"e"},
-                 {"f"},        {"f#", "gb"}, {"g"},        {"g#", "ab"}};
+char *NOTES[12][2] = {{"a", NULL},        {"a#", "bb", NULL}, {"b", NULL},        {"c"},
+                      {"c#", "db"}, {"d"},        {"d#", "eb"}, {"e"},
+                      {"f"},        {"f#", "gb"}, {"g"},        {"g#", "ab"}};
 
-int which_note(char **note) {
-  int cmt = 0;
-    while (NOTES[cmt] != note && cmt<12) {
-        int j = 0;
-        while (NOTES[cmt][j] != '\0') {
-            if (NOTES[cmt][j] == note) {
-                return cmt+1;
-            }
-            j++;
-        }
-        cmt++;
+int which_note(char *note) {
+  for (int cmt = 0; cmt < 12; cmt++) {
+    int j = 0;
+    while (*NOTES[cmt][j] != NULL) {
+      if (*NOTES[cmt][j] == *note) {
+        return cmt;
+      }
+      j++;
+    }
+  }
+  printf("error : unwnown note");
+  return -1;
 }
 
 int main() {
   printf("==== Chord detector ====\n");
   printf(
-      "Give the notes of the chord, on a single line, separated by spaces :\n");
+      "Give the notes of the chord, on a single line, separated by spaces\n");
   char line[71];
-  tolowercase(line);
   // char *line = malloc(sizeof(char)*10);
   scanf(" %[^\n]", line);
-  char **notes = splitnlower(line, ' ');
+  tolowercase(line);
+  char **notes = split(line, ' ');
+
   int i = 0;
-
-
+  printf("%d\n", which_note(notes[0]));
+  /*
   while (notes[i] != '\0' && notes[i] != NULL) {
+    notes[i] = which_note(notes[i]);
+  }
 
-  printf("%s , %s\n", notes[0], notes[1]);
+  printf("%s , %s\n", notes[0], notes[1])*/
+
+  free(notes);
   return 0;
 }
